5e991fa017968ddef49c7f727e5998c6514e86fe
Natalie Bunduwongse <nf1993@gmail.com>
2024-04-05 11:43:33 +0700

    fix: contracts

--- contracts/contracts/RunnerToken.sol
+++ contracts/contracts/RunnerToken.sol
@@ -9,37 +9,39 @@ import "@imtbl/contracts/contracts/access/MintingAccessControl.sol";
 import "@imtbl/contracts/contracts/token/erc721/preset/ImmutableERC721.sol";
 
 contract RunnerToken is ERC20, ERC20Burnable, MintingAccessControl {
-  // A reference to the Immutable Runner Skin contract for the craftSkin function to use.
-  // Note: Immutable Runner Skin contract simply extends ImmutableERC721, so we can set
-  // the type to ImmutableERC721
-  ImmutableERC721 private _skinContract;
+    // A reference to the Immutable Runner Skin contract for the craftSkin function to use.
+    // Note: Immutable Runner Skin contract simply extends ImmutableERC721, so we can set
+    // the type to ImmutableERC721
+    ImmutableERC721 private _skinContract;
 
-  constructor(address skinContractAddr) ERC20("Immutable Runner Token", "IMR") {
-    // Grant the contract deployer the default admin role: it will be able
-    // to grant and revoke any roles
-    _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
-    // Save the Immutable Runner Skin contract address
-    _skinContract = ImmutableERC721(skinContractAddr);
+    constructor(
+        address skinContractAddr
+    ) ERC20("Immutable Runner Token", "IMR") {
+        // Grant the contract deployer the default admin role: it will be able
+        // to grant and revoke any roles
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        // Save the Immutable Runner Skin contract address
+        _skinContract = ImmutableERC721(skinContractAddr);
 
-    // Uncomment the line below to grant minter role to contract deployer
-    // _grantRole(MINTER_ROLE, msg.sender);
-  }
+        // Uncomment the line below to grant minter role to contract deployer
+        // _grantRole(MINTER_ROLE, msg.sender);
+    }
 
-  // Mints the number of tokens specified
-  function mint(address to, uint256 amount) external onlyRole(MINTER_ROLE) {
-    _mint(to, amount);
-  }
+    // Mints the number of tokens specified
+    function mint(address to, uint256 amount) external onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
+    }
 
-  // Burns three tokens and crafts a skin to the caller
-  function craftSkin() external {
-    uint256 numTokens = 3 * 10 ** decimals();
+    // Burns three tokens and crafts a skin to the caller
+    function craftSkin() external {
+        uint256 numTokens = 3 * 10 ** decimals();
 
-    // Burn caller's three tokens
-    _burn(msg.sender, numTokens);
+        // Burn caller's three tokens
+        _burn(msg.sender, numTokens);
 
-    // Mint one Immutable Runner Skin to the caller
-    // Note: To mint a skin, the Immutable Runner Token contract must have the
-    // Immutable Runner Skin contract minter role.
-    _skinContract.mintByQuantity(msg.sender, 1);
-  }
+        // Mint one Immutable Runner Skin to the caller
+        // Note: To mint a skin, the Immutable Runner Token contract must have the
+        // Immutable Runner Skin contract minter role.
+        _skinContract.mintByQuantity(msg.sender, 1);
+    }
 }
