1e859dda06eb4e4fb37a9852594033745fb48bce
Natalie Bunduwongse <nf1993@gmail.com>
2024-09-20 14:32:24 +1200

    fix: skin

--- Assets/Runner/Scripts/MemoryCache.cs
+++ Assets/Runner/Scripts/MemoryCache.cs
@@ -2,7 +2,6 @@ namespace HyperCasual.Runner
 {
     internal class MemoryCache
     {
-        public static bool s_UseCoolerSkin = false;
         public static int s_CurrentLevel = 0; // First level is 1
     }
 }
--- Assets/Runner/Scripts/PlayerController.cs
+++ Assets/Runner/Scripts/PlayerController.cs
@@ -165,9 +165,7 @@ namespace HyperCasual.Runner
 
             ResetSpeed();
 
-            m_Fox.GetComponent<Renderer>().material.SetTexture("_BaseMap",
-                MemoryCache.s_UseCoolerSkin ? m_foxGradientTexture :
-                SaveManager.Instance.UseNewSkin ? m_foxBlueTexture : m_foxOriginalTexture);
+            m_Fox.GetComponent<Renderer>().material.SetTexture("_BaseMap", SaveManager.Instance.UseNewSkin ? m_foxBlueTexture : m_foxOriginalTexture);
         }
 
         /// <summary>
