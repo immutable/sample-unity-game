e169b788fe1f8db702b466a1b30e0b1626c01042
Natalie Bunduwongse <nf1993@gmail.com>
2024-10-02 19:10:32 +1300

    fix: dependency

--- Assets/Shared/Scripts/UI/AssetDetailsView.cs
+++ Assets/Shared/Scripts/UI/AssetDetailsView.cs
@@ -368,8 +368,8 @@ namespace HyperCasual.Runner
             var message = signableAction.GetSignableAction().Message;
 
             // Use Unity Passport package to sign typed data function to sign the listing payload
-            return await Passport.Instance.ZkEvmSignTypedDataV4(
-                      JsonConvert.SerializeObject(message, Formatting.Indented));
+            return "";//await Passport.Instance.ZkEvmSignTypedDataV4(
+                      //JsonConvert.SerializeObject(message, Formatting.Indented));
         }
 
         /// <summary>
