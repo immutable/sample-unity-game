804c94ee0f9c8dc5c0295b04fdeff3e46515c377
Natalie Bunduwongse <nf1993@gmail.com>
2024-10-22 13:46:27 +1300

    style: fix lint

--- contracts/scripts/deploy.ts
+++ contracts/scripts/deploy.ts
@@ -1,22 +1,23 @@
 import { ethers } from 'hardhat'; // eslint-disable-line import/no-extraneous-dependencies
 
 async function main() {
-  // Load the Immutable Runner Tokencontract and get the contract factory
-  // const tokenContractFactory = await ethers.getContractFactory('RunnerToken');
+  // Load the Immutable Runner Token contract and get the contract factory
+  const tokenContractFactory = await ethers.getContractFactory('RunnerToken');
 
   // Deploy the contract to the zkEVM network
-  // const tokenContract = await tokenContractFactory.deploy(
-  //   'YOUR_IMMUTABLE_RUNNER_SKIN_CONTRACT_ADDRESS', // Immutable Runner Skin contract address
-  // );
+  const tokenContract = await tokenContractFactory.deploy(
+    'YOUR_IMMUTABLE_RUNNER_SKIN_CONTRACT_ADDRESS', // Immutable Runner Skin contract address
+  );
 
-  // console.log('Contract deployed to:', await tokenContract.getAddress());
+  console.log('Contract deployed to:', await tokenContract.getAddress());
 
+  // Load the Immutable Runner Pack contract and get the contract factory
   const packContractFactory = await ethers.getContractFactory('RunnerPack');
 
   // Deploy the contract to the zkEVM network
   const packContract = await packContractFactory.deploy(
-    '0xb237501b35dfdcad274299236a141425469ab9ba', // Immutable Runner Token contract address
-    '0x6b969FD89dE634d8DE3271EbE97734FEFfcd58eE' // Immutable Operator Allowlist
+    'YOUR_IMMUTABLE_RUNNER_TOKEN_CONTRACT_ADDRESS', // Immutable Runner Token contract address
+    '0x6b969FD89dE634d8DE3271EbE97734FEFfcd58eE', // Immutable Operator Allowlist
   );
 
   console.log('Contract deployed to:', await packContract.getAddress());
