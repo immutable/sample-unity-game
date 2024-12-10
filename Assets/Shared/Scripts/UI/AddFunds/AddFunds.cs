884b1a16a439f5b0accdcd0ca990398c0dfaae32
Natalie Bunduwongse <nf1993@gmail.com>
2024-12-10 16:42:02 +1300

    feat: add funds and transak view

---
+++ Assets/Shared/Scripts/UI/AddFunds/AddFunds.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Linq;
+using Immutable.Marketplace.OnRamp;
+using Immutable.Passport;
+
+namespace HyperCasual.Runner
+{
+    /// <summary>
+    /// Provides various options for players to add funds to the game.
+    /// </summary>
+    public class AddFunds : MonoBehaviour
+    {
+        [SerializeField] GameObject m_Options;
+        [SerializeField] Button m_FiatButton;
+        [SerializeField] Button m_TokenButton;
+        [SerializeField] Button m_CancelButton;
+        
+        [SerializeField] TransakView m_TransakView;
+
+        public void Show()
+        {
+            m_FiatButton.onClick.RemoveListener(OnFiatButtonClicked);
+            m_FiatButton.onClick.AddListener(OnFiatButtonClicked);
+            
+            m_TokenButton.onClick.RemoveListener(OnTokenButtonClicked);
+            m_TokenButton.onClick.AddListener(OnTokenButtonClicked);
+            
+            m_CancelButton.onClick.RemoveListener(OnCancelButtonClicked);
+            m_CancelButton.onClick.AddListener(OnCancelButtonClicked);
+            
+            m_Options.gameObject.SetActive(true);
+            m_TransakView.gameObject.SetActive(false);
+            gameObject.SetActive(true);
+        }
+
+        private async void OnFiatButtonClicked()
+        {
+            string environment = Immutable.Passport.Model.Environment.SANDBOX;
+            string email = await Passport.Instance.GetEmail();
+            List<string> walletAddress = await Passport.Instance.ZkEvmRequestAccounts();
+            OnRamp onRamp = new OnRamp(environment, email, walletAddress.FirstOrDefault());
+            string link = await onRamp.GetLink();
+            Debug.Log($"onRamp.GetOnRampLink: {link}");
+
+            m_TransakView.Show(link, () =>
+            {
+                m_Options.gameObject.SetActive(true);
+                m_TransakView.gameObject.SetActive(false);
+            });
+        }
+
+        private void OnTokenButtonClicked()
+        {
+            Application.OpenURL($"https://checkout-playground.sandbox.immutable.com/embedded/add-funds/?publishableKey=pk_imapik-test-Xdera%40&passportClientId={Config.CLIENT_ID}&redirectUri={Config.REDIRECT_URI}");
+        }
+
+        private void OnCancelButtonClicked()
+        {
+            gameObject.SetActive(false);
+            m_Options.gameObject.SetActive(true);
+            m_TransakView.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Shared/Scripts/UI/AddFunds/TransakView.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using Vuplex.WebView;
+
+namespace HyperCasual.Runner
+{
+    /// <summary>
+    /// Loads the Transak on-ramp widget, allowing players to purchase tokens 
+    /// using fiat currencies (e.g., USD).
+    /// </summary>
+    public class TransakView : MonoBehaviour
+    {
+        [SerializeField] Button m_CloseButton;
+        [SerializeField] CanvasWebViewPrefab m_WebViewPrefab;
+
+        private string m_Url = "https://www.immutable.com";
+
+        public void Show(string url, UnityAction onClose)
+        {
+            m_CloseButton.onClick.RemoveListener(onClose);
+            m_CloseButton.onClick.AddListener(onClose);
+            
+            m_Url = url;
+            m_WebViewPrefab.InitialUrl = m_Url;
+            
+            gameObject.SetActive(true);
+        }
+    }
+}
